cf7424c208f6c67d19ed0f087a46acdbb2d09298
benesamaritan <243715333+benesamaritan@users.noreply.github.com>
2026-01-20 03:35:12 +0700

    use matugen from dms rather than catppuccin, and fix tab_bar background

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,29 +1,31 @@
 local wezterm = require "wezterm"
 
+local colors = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/dank-theme.toml")
 wezterm.add_to_config_reload_watch_list(wezterm.config_dir .. "/colors/dank-theme.toml")
-
--- wezterm.on('update-right-status', function(window, pane)
---   window:set_left_status 'left'
---   window:set_right_status 'right'
--- end)
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 
 local config = {
   -- initial_cols = 120,
   -- initial_rows = 28,
-  font_size = 15,
+  font_size = 13,
   font = wezterm.font "JetBrains Mono",
   -- color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
   color_scheme = "dank-theme",
-  window_padding = {
-    left = 10,
-    right = 10,
-    top = 14,
-    bottom = 14,
+  colors = {
+    tab_bar = {
+      background = colors.ansi[1] or "transparent",
+    },
   },
-  window_background_opacity = 0.7,
+  window_padding = {
+    left = 4,
+    right = 4,
+    top = 6,
+    bottom = 2,
+  },
+  window_background_opacity = 0.85,
   -- text_background_opacity = 0.5,
   use_fancy_tab_bar = false,
-  hide_tab_bar_if_only_one_tab = false,
+  hide_tab_bar_if_only_one_tab = true,
   tab_bar_at_bottom = true,
   show_new_tab_button_in_tab_bar = false,
   tab_max_width = 25,
@@ -33,40 +35,36 @@ local config = {
   },
 }
 
-local colors = wezterm.color.load_scheme(wezterm.config_dir .. "/colors/dank-theme.toml")
-
-local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 tabline.setup({
- options = {
+  options = {
     theme = colors,
-    -- theme_overrides = {
-    --   normal_mode = {
-    --     a = { fg = colors.foreground, bg = colors.ansi[2] },
-    --     b = { fg = colors.ansi[2], bg = colors.background },
-    --     c = { fg = colors.foreground, bg = colors.background },
-    --   },
-    --   copy_mode = {
-    --     a = { fg = colors.background, bg = colors.ansi[3] },
-    --     b = { fg = colors.ansi[3], bg = colors.background },
-    --     c = { fg = colors.foreground, bg = colors.background },
-    --   },
-    --   search_mode = {
-    --     a = { fg = colors.background, bg = colors.ansi[4] },
-    --     b = { fg = colors.ansi[4], bg = colors.background },
-    --     c = { fg = colors.foreground, bg = colors.background },
-    --   },
-    --   window_mode = {
-    --     a = { fg = colors.background, bg = colors.ansi[5] },
-    --     b = { fg = colors.ansi[5], bg = colors.background },
-    --     c = { fg = colors.foreground, bg = colors.background },
-    --   },
-    --   tab = {
-    --     active = { fg = colors.ansi[7], bg = colors.background },
-    --     inactive = { fg = colors.foreground, bg = colors.background },
-    --     inactive_hover = { fg = colors.brights[7], bg = colors.background },
-    --   }
-    -- },
-   section_separators = {
+    theme_overrides = {
+      normal_mode = {
+        a = { fg = colors.ansi[5], bg = colors.ansi[1] },
+        x = { fg = colors.ansi[5], bg = colors.ansi[9] },
+        y = { fg = colors.ansi[1], bg = colors.ansi[6] },
+      },
+      -- copy_mode = {
+      --   a = { fg = colors.ansi[4], bg = colors.ansi[1] },
+      --   x = { fg = colors.ansi[4], bg = colors.ansi[1] },
+      --   y = { fg = colors.ansi[1], bg = colors.ansi[8] },
+      -- },
+      -- search_mode = {
+      --   a = { fg = colors.ansi[4], bg = colors.ansi[1] },
+      --   x = { fg = colors.ansi[4], bg = colors.ansi[1] },
+      --   y = { fg = colors.ansi[1], bg = colors.ansi[8] },
+      -- },
+      -- window_mode = {
+      --   a = { fg = colors.background, bg = colors.ansi[7] },
+      --   b = { fg = colors.ansi[5], bg = colors.background },
+      -- },
+      tab = {
+        active = { fg = colors.ansi[1], bg = colors.ansi[5]},
+        inactive = { fg = colors.ansi[5], bg = colors.ansi[1]},
+        inactive_hover = { fg = colors.ansi[5], bg = colors.ansi[1] },
+      }
+    },
+    section_separators = {
       left = wezterm.nerdfonts.ple_right_half_circle_thick,
       right = wezterm.nerdfonts.ple_left_half_circle_thick,
     },
@@ -80,35 +78,34 @@ tabline.setup({
     },
   },
   sections = {
-    tabline_b = {
-      {
-        'workspace',
-        icons_only = true,
-      }
-    },
+    tabline_b = {''},
     tab_active = {
       {
-        'index',
-        zero_indexed = true,
+        'parent',
+        max_length = 4,
+        padding = 0
       },
-      { 'parent', padding = 0 },
       '/',
       {
         'cwd',
         padding = { left = 0, right = 1 },
-        max_length = 8,
+        max_length = 6,
       },
-      { 'zoomed', padding = 0 },
-    },
-    tabline_z = {
       {
-        'domain',
+        'zoomed',
+        padding = 0
+      },
+    },
+    tabline_y = {
+      'datetime',
+      {
+        'battery',
         icons_only = true,
       }
     },
+    tabline_z = {{''}},
   },
 })
-tabline.set_theme(colors)
 
 wezterm.on("window-config-reloaded", function(window, pane)
   tabline.set_theme(colors)
